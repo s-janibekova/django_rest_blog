e91bf01d172c60e7ea984f60a211bf30dbdbbc78
s-janibekova <49087652+s-janibekova@users.noreply.github.com>
2021-05-27 14:18:45 +0600

    secret_key_remove

--- blogapi/config/settings.py
+++ blogapi/config/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '8(g_*0#h7*c7_reu0to$-!1&4=j_j(qv!++&i*6*2&*bc3$5_*'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
